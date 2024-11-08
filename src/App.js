49c2eb766f2521f166c89e04e7af80bb846daef4
Ion Cretu <ion.cretu@adorsys.com>
2024-11-08 15:13:11 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -8,12 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           learn react
-        </p>
-        <p>
-          learn react
-        </p>
-
-       
+        </p>      
       </header>
     </div>
   );
